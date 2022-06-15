22e0d1acd21839cd33c85f0e76f54456a4938b15
Gius993 <giuseppevidili9@gmail.com>
2022-06-15 15:36:22 +0200

    automazione di ieri inserita nella funzione

--- js/script.js
+++ js/script.js
@@ -24,59 +24,42 @@
  //se l'array è piena gameEnd - 16 ---> alert hai vinto
 
  //A
- const userLevel = prompt('Dimmi livello da 1 a 3');
+
+ const playBtn = document.querySelector('#play-game');
+ playBtn.addEventListener('click', startGame);
+
+ function startGame() {
+	const userLevel = prompt('Dimmi livello da 1 a 3');
   
- let gameEnd;
- // B
+	let gameEnd;
+	// B
+   
+   if(userLevel === '1') {
+	   gameEnd = 100;
+   }else if(userLevel === '2'){
+	   gameEnd = 81;
+   }else if(userLevel === '3') {
+	   gameEnd = 49;
+	   
+   }
+   
+   
+   // C Funzione 1
+   const bomb = generatedBomb(16, 1, gameEnd);
+   console.log(bomb);
+   function generatedBomb(bombNumber, minBomb, maxBomb){
+	   const randomBombArray = [];
+	   while(randomBombArray.length < bombNumber) {
+		   const randomNumber = getRndInteger(minBomb, maxBomb);
+		   if(!randomBombArray.includes(randomNumber)){
+			   randomBombArray.push(randomNumber);
+		   }
+	   }
+	   console.log(randomBombArray);
+	   return randomBombArray;
+   }
+ }
 
-if(userLevel === '1') {
-	gameEnd = 100;
-}else if(userLevel === '2'){
-	gameEnd = 81;
-}else if(userLevel === '3') {
-	gameEnd = 49;
-	
-}
-
-
-// C Funzione 1
-const bomb = generatedBomb(16, 1, gameEnd);
-console.log(bomb);
-function generatedBomb(bombNumber, minBomb, maxBomb){
-	const randomBombArray = [];
-	while(randomBombArray.length < bombNumber) {
-		const randomNumber = getRndInteger(minBomb, maxBomb);
-		if(!randomBombArray.includes(randomNumber)){
-			randomBombArray.push(randomNumber);
-		}
-	}
-	console.log(randomBombArray);
-	return randomBombArray;
-}
-function getRndInteger(min, max) {
+ function getRndInteger(min, max) {
 	return Math.floor(Math.random() * (max - min + 1) ) + min;
-}
-// D
-const endGame = gameEnd - 16;
-let gamePlay = true;
-const numberCorrect = [];
-while(gamePlay){
-	const userNumber = parseInt(prompt('numero'));
-	
-	//use = bomba
-	if(bomb.includes(userNumber)){
-		gamePlay = false;
-		alert('Perdi');
-	} else {
-		
-		if(!numberCorrect.includes(userNumber)){
-			
-			numberCorrect.push(userNumber);
-		}
-		if (numberCorrect.length === endGame){
-			gamePlay = false;
-			alert('Vinci');
-		}
-	} 
-
 }
